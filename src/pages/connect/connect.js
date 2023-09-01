525504cb80fb7a3a6f60d057a79d9954e256f10a
Shirov Ivan <117617823+Carnifex111@users.noreply.github.com>
2023-09-02 04:10:05 +0500

    form

--- src/pages/connect/connect.js
+++ src/pages/connect/connect.js
@@ -1,5 +1,30 @@
+import "./connect.scss";
+
 const ConnectPage = () => {
-  return <div className="page">ConnectPage</div>;
+  return (
+    <div className="connent">
+      <form className="connent-form">
+        <div className="connent-form-title">Сontact us</div>
+        <div className="input-group">
+          <label htmlFor="email">Your Email</label>
+          <input type="email" className="input" name="email" />
+        </div>
+        <div class="input-group">
+          <label htmlFor="email">Your Task</label>
+          <textarea
+            className="input no-resize"
+            name="task"
+            cols="40"
+            rows="20"
+            required
+          ></textarea>
+        </div>
+        <button type="submit" className="submit-button">
+          Submit
+        </button>
+      </form>
+    </div>
+  );
 };
 
 export default ConnectPage;
